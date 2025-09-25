d0fe2f7ce613f1d311841df35cc55a66b155c54e
GreenVGJR <greenvgjrgaming@gmail.com>
2025-09-26 04:41:28 +0700

    Something

--- back/client/youtubeConfig.js
+++ back/client/youtubeConfig.js
@@ -1,14 +1,17 @@
 // Test replace yt stream
-let vt;
 async function fallbackYTStream(lstracks) {
     try {
-    vt=vt?vt:await fetch('https://youtubei.googleapis.com/youtubei/v1/player?prettyPrint=false&fields=responseContext.visitorData',{method:"POST",headers:{"Content-Type":"application/json","Accept-Encoding":"gzip"},body:JSON.stringify({context:{client:{clientName:"ANDROID_VR",clientVersion:"1.00",hl:"en",gl:"US"}}})}).then(r=>r.json()).then(j=>j?.responseContext?.visitorData).catch(()=>undefined);const pl={videoId:lstracks.split('watch?v=')[1],context:{client:{hl:"en",gl:"US",clientName:"ANDROID_VR",clientVersion:"1.00",androidSdkVersion:38,visitorData:vt,clientScreen:"WATCH",clientFormFactor:"UNKNOWN_FORM_FACTOR"},request:{useSsl:true,internalExperimentFlags:[],consistencyTokenJars:[]}},playbackContext:{contentPlaybackContext:{vis:0,splay:true,html5Preference:"HTML5_PREF_WANTS",lactMilliseconds:"-1"}},attestationRequest:{omitBotguardData:true},racyCheckOk:true,contentCheckOk:true};
+    const pl={videoId:lstracks.split('watch?v=')[1],context:{client:{hl:"en",gl:"US",clientName:"WEB",clientVersion:"2.20250926",clientScreen:"WATCH",clientFormFactor:"UNKNOWN_FORM_FACTOR"},request:{useSsl:true,internalExperimentFlags:[],consistencyTokenJars:[]}},playbackContext:{contentPlaybackContext:{vis:0,splay:true,html5Preference:"HTML5_PREF_WANTS",lactMilliseconds:"-1"}},attestationRequest:{omitBotguardData:true},racyCheckOk:true,contentCheckOk:true};
 
-    let a = await fetch('https://youtubei.googleapis.com/youtubei/v1/player?prettyPrint=false&fields=streamingData(hlsManifestUrl,adaptiveFormats(itag,url))', { method: "POST", body: JSON.stringify(pl), headers: { "Content-Type": "application/json", "Accept-Encoding": "gzip"}}).then(b => b.json());
-    if(a.streamingData?.hlsManifestUrl) { return a.streamingData.hlsManifestUrl; }
-    a = a.streamingData?.adaptiveFormats?.find(c => c.itag === 251 || c.itag === 140); //Known issue, throttled
-    if(!a.url) { throw new Error(); }
-        return a.url;
+    let a = await fetch('https://youtubei.googleapis.com/youtubei/v1/player?prettyPrint=false&fields=streamingData.hlsManifestUrl', { method: "POST", body: JSON.stringify(pl), headers: { "Content-Type": "application/json", "Accept-Encoding": "gzip", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15,gzip(gfe)"}}).then(b => b.json());
+
+    if(!a.streamingData?.hlsManifestUrl) { throw new Error(); }
+    const b = await fetch(a.streamingData.hlsManifestUrl, { method: "GET"}).then(c => c.text());
+
+    const bc = b.split("https://manifest.googlevideo.com");
+    const c = bc[bc.length - 1];
+    
+    return "https://manifest.googlevideo.com" + c;
     }
     catch {
         throw new Error();
